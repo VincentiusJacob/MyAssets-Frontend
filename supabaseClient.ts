102c50a3b4c0f231226ef6c8422a89c7a3196247
VincentiusJacob <142817267+VincentiusJacob@users.noreply.github.com>
2024-12-18 21:02:33 +0700

    latest commit

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_SERVICEKEY as string
+  process.env.REACT_APP_SUPABASE_URL as string,
+  process.env.REACT_APP_SUPABASE_SERVICEKEY as string
 );
